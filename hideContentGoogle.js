28a21668c7a926395ce94c36102855715b70465c
Bryan Yee <yee.bryan.c@gmail.com>
2016-06-16 17:54:14 -0700

    Added a short delay before executing the 'hide'

--- hideContentGoogle.js
+++ hideContentGoogle.js
@@ -1,4 +1,11 @@
-
+/*
 window.onload = function(){
 	document.getElementById("taw").style.display = "none";
+}
+*/
+
+var hide = setTimeout(hide,500);
+
+function hide(){
+	document.getElementById("taw").style.display = "none";	
 }
