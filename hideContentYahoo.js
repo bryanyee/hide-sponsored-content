551dac3d2e15eaa8a9e0846bd65b2d4cf57bb0f7
Bryan Yee <yee.bryan.c@gmail.com>
2016-06-17 17:54:17 -0700

    Hides more Yahoo content

--- hideContentYahoo.js
+++ hideContentYahoo.js
@@ -1,8 +1,11 @@
 function hideSponsoredContent(){
+	var topBannerAd = document.getElementById("Billboard-ad");
 	var sponsoredContent = Array.from(document.getElementsByClassName("js-stream-ad  Wow(bw) Pos(r) Mx(a) Mt(-1px) Bxz(bb) Bgc(#fff)"));
 	var sponsoredVideoContent = Array.from(document.getElementsByClassName("js-stream-ad js-video-content Wow(bw) Pos(r) Mx(a) Mt(-1px) Bxz(bb) Bgc(#fff)"));
-	sponsoredContent.forEach( element => {element.style.display = "none"} );
-	sponsoredVideoContent.forEach( element => {element.style.display = "none"} );
+	var sponsoredSideContent1 = Array.from(document.getElementsByClassName("Whs-nw  sponsor-color ad-sponsor-color")).map( element => element.parentNode.parentNode.parentNode );
+	var sponsoredSideContent2 = Array.from(document.getElementsByClassName("Mend(6px) C(#b9bdc5)")).map( element => element.parentNode.parentNode.parentNode.parentNode.parentNode );
+	var allSponsoredContent = sponsoredContent.concat(sponsoredVideoContent).concat(sponsoredSideContent1).concat(sponsoredSideContent2).concat(topBannerAd);
+	allSponsoredContent.forEach( element => {element.style.display = "none"} );
 }
 
 hideSponsoredContent();
